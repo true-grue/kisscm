ffe2f00bed8c157ce38778753d42589f9ee3221e
Peter Sovietov <peter.sovietov@gmail.com>
2021-12-09 21:42:21 +0300

    добавления от CalamityDeadshot

--- filters/upper.lua
+++ filters/upper.lua
@@ -1,11 +1,21 @@
--- we use preloaded text to get a UTF-8 aware 'upper' function
 local text = require('text')
 
 function Header(el)
     if el.level == 1 and FORMAT == 'docx' then
-      return pandoc.walk_block(el, {
+        return pandoc.walk_block(el, {
         Str = function(el)
             return pandoc.Str(text.upper(el.text))
         end })
     end
+end
+
+function Blocks(blocks)
+    local hblocks = {}
+    for i, el in pairs(blocks) do
+        if el.t == "Header" and el.level == 1 then
+            table.insert(hblocks, pandoc.RawBlock("tex","\\newpage"))
+        end
+        table.insert(hblocks, el)
+    end
+    return hblocks
 end
